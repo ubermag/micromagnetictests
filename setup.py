e8a923934b2894129bd37d55fe9a1c71cd9ba6c9
Marijan Beg <m.beg@soton.ac.uk>
2020-06-14 12:23:04 +0100

    Update version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setuptools.setup(
     name='micromagnetictests',
-    version='0.1.1',
+    version='0.1.2',
     description=('Micromagnetic tests for Ubermag calculators'),
     long_description=long_description,
     long_description_content_type='text/markdown',
