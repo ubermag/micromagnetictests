41d22d20fb633c86d81a31350f1a41d3f574cc47
Marijan Beg <m.beg@soton.ac.uk>
2020-06-27 20:05:20 +0100

    Improve test.

--- micromagnetictests/calculatortests/rkky.py
+++ micromagnetictests/calculatortests/rkky.py
@@ -17,6 +17,8 @@ class TestRKKY:
         self.subregions = {'r1': df.Region(p1=(-5e-9, -5e-9, -3e-9),
                                            p2=(5e-9, 0, 3e-9)),
                            'r2': df.Region(p1=(-5e-9, 0, -3e-9),
+                                           p2=(5e-9, 1e-9, 3e-9)),
+                           'r3': df.Region(p1=(-5e-9, 1e-9, -3e-9),
                                            p2=(5e-9, 5e-9, 3e-9))}
 
     def m_init(self, pos):
@@ -35,7 +37,7 @@ class TestRKKY:
 
         system = mm.System(name=name)
         system.energy = mm.RKKY(sigma=sigma, sigma2=sigma2,
-                                subregions=['r1', 'r2'])
+                                subregions=['r1', 'r3'])
 
         mesh = df.Mesh(region=self.region, n=self.n,
                        subregions=self.subregions)
@@ -46,7 +48,7 @@ class TestRKKY:
 
         # AFM
         m1 = system.m.orientation((0, -0.5e-9, 0))
-        m2 = system.m.orientation((0, 0.5e-9, 0))
+        m2 = system.m.orientation((0, 1.5e-9, 0))
         assert abs(np.dot(m1, m2) - (-1)) < 1e-3
 
         system.energy.rkky.sigma = 1e4
@@ -58,7 +60,7 @@ class TestRKKY:
 
         # FM
         m1 = system.m.orientation((0, -0.5e-9, 0))
-        m2 = system.m.orientation((0, 0.5e-9, 0))
+        m2 = system.m.orientation((0, 1.5e-9, 0))
         assert abs(np.dot(m1, m2) - 1) < 1e-3
 
         self.calculator.delete(system)
