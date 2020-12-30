15fa650a00d6e95c1833401345ab694131b011f7
marijanbeg <m.beg@soton.ac.uk>
2020-12-30 15:50:31 +0000

    Update version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setuptools.setup(
     name='micromagnetictests',
-    version='0.1.5',
+    version='0.1.6',
     description='Testing package for computational magnetism tools.',
     author='Marijan Beg and Hans Fangohr',
     long_description=long_description,
