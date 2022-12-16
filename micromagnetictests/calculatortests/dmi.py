Fix: field.average -> field.mean()

micromagnetictests/calculatortests/dmi.py
@@ -49,7 +49,7 @@ class TestDMI:
 
         # There are 4N cells in the mesh. Because of that the average should be
         # 0.
-        assert np.linalg.norm(system.m.average) < 1
+        assert np.linalg.norm(system.m.mean()) < 1
 
         self.calculator.delete(system)
 
@@ -75,10 +75,10 @@ class TestDMI:
             md = self.calculator.MinDriver()
         md.drive(system)
 
-        assert np.linalg.norm(system.m["r1"].average) > 1
+        assert np.linalg.norm(system.m["r1"].mean()) > 1
         # There are 4N cells in the region with D!=0. Because of that
         # the average should be 0.
-        assert np.linalg.norm(system.m["r2"].average) < 1
+        assert np.linalg.norm(system.m["r2"].mean()) < 1
 
         self.calculator.delete(system)
 
@@ -117,7 +117,7 @@ class TestDMI:
 
             # There are 4N cells in the mesh. Because of that the
             # average should be 0.
-            assert np.linalg.norm(system.m.average) < 1
+            assert np.linalg.norm(system.m.mean()) < 1
 
         self.calculator.delete(system)
 

micromagnetictests/calculatortests/exchange.py
@@ -41,7 +41,7 @@ class TestExchange:
         md = self.calculator.MinDriver()
         md.drive(system)
 
-        assert abs(np.linalg.norm(system.m.average) - Ms) < 1
+        assert abs(np.linalg.norm(system.m.mean()) - Ms) < 1
 
         self.calculator.delete(system)
 
@@ -61,15 +61,15 @@ class TestExchange:
         md.drive(system)
 
         # r1
-        assert abs(np.linalg.norm(system.m["r1"].average) - Ms) < 1
+        assert abs(np.linalg.norm(system.m["r1"].mean()) - Ms) < 1
         # r2
-        assert abs(np.linalg.norm(system.m["r2"].average) - Ms) < 1
+        assert abs(np.linalg.norm(system.m["r2"].mean()) - Ms) < 1
 
         assert (
             abs(
                 np.dot(
-                    system.m["r1"].orientation.average,
-                    system.m["r2"].orientation.average,
+                    system.m["r1"].orientation.mean(),
+                    system.m["r2"].orientation.mean(),
                 )
                 - (-1)
             )
@@ -100,6 +100,6 @@ class TestExchange:
         md = self.calculator.MinDriver()
         md.drive(system)
 
-        assert abs(np.linalg.norm(system.m.average) - Ms) < 1
+        assert abs(np.linalg.norm(system.m.mean()) - Ms) < 1
 
         self.calculator.delete(system)

micromagnetictests/calculatortests/fixedsubregions.py
@@ -34,8 +34,8 @@ class TestFixedSubregions:
         md = self.calculator.MinDriver()
         md.drive(system, fixed_subregions=["r1"])
 
-        assert np.linalg.norm(np.subtract(system.m["r1"].average, (Ms, 0, 0))) < 1
+        assert np.linalg.norm(np.subtract(system.m["r1"].mean(), (Ms, 0, 0))) < 1
 
-        assert np.linalg.norm(np.subtract(system.m["r2"].average, (0, 0, Ms))) < 1
+        assert np.linalg.norm(np.subtract(system.m["r2"].mean(), (0, 0, Ms))) < 1
 
         self.calculator.delete(system)

micromagnetictests/calculatortests/zeeman.py
@@ -178,9 +178,9 @@ class TestZeeman:
         md = self.calculator.MinDriver()
         md.drive(system)
 
-        assert np.linalg.norm(np.subtract(system.m["r1"].average, (Ms, 0, 0))) < 1
+        assert np.linalg.norm(np.subtract(system.m["r1"].mean(), (Ms, 0, 0))) < 1
 
-        assert np.linalg.norm(np.subtract(system.m["r2"].average, (0, 0, Ms))) < 1
+        assert np.linalg.norm(np.subtract(system.m["r2"].mean(), (0, 0, Ms))) < 1
 
     def test_time_dict(self):
         name = "zeeman_dict"
@@ -197,9 +197,9 @@ class TestZeeman:
         md = self.calculator.MinDriver()
         md.drive(system)
 
-        assert np.linalg.norm(np.subtract(system.m["r1"].average, (Ms, 0, 0))) < 1
+        assert np.linalg.norm(np.subtract(system.m["r1"].mean(), (Ms, 0, 0))) < 1
 
-        assert np.linalg.norm(np.subtract(system.m["r2"].average, (0, 0, Ms))) < 1
+        assert np.linalg.norm(np.subtract(system.m["r2"].mean(), (0, 0, Ms))) < 1
 
         # time-dependent - sin
         system.energy = mm.Zeeman(H=H, func="sin", f=1e9, t0=1e-12)